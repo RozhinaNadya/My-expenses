a2a05c822cdc0e7c998267d7e26da5c14afb8f53
RozhinaNadya <zhuk.09@mail.ru>
2022-07-11 15:51:02 +0200

    Initial Commit

---
+++ MyExpenses/MyExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyExpensesApp.swift
+//  MyExpenses
+//
+//  Created by Надежда on 2022-07-11.
+//
+
+import SwiftUI
+
+@main
+struct MyExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
