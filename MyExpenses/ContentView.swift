3d926e6cc07069f57829c3dc048fb0356520aeba
RozhinaNadya <zhuk.09@mail.ru>
2022-07-12 11:08:31 +0200

    Added data storage

--- MyExpenses/ContentView.swift
+++ MyExpenses/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ExpenseItem: Identifiable {
+struct ExpenseItem: Identifiable, Codable {
     let id = UUID()
     let name: String
     let type: String
@@ -15,7 +15,25 @@ struct ExpenseItem: Identifiable {
 }
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        //заархивируем данные:
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "items")
+            }
+        }
+    }
+    //разархивируем данные, чтобы можно было прочитать:
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "items") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+    }
 }
 
 struct ContentView: View {
